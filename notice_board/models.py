c2fd3e4281fff40e63d63f54065c0b5f9b2d14db
petrovi4 <40.10.444@gmail.com>
2024-08-20 10:21:31 +0300

    add models

--- notice_board/models.py
+++ notice_board/models.py
@@ -1,17 +1,28 @@
 from django.db import models
 
+from users.models import User
+
 
 class Ad(models.Model):
-    title = models.CharField(max_length=200, verbose_name='название товара')
+    """
+    Модель, представляющая объявления.
+    """
+    title = models.CharField(
+        max_length=200,
+        verbose_name='название товара',
+        help_text= 'Заголовок объявления'
+    )
     price = models.IntegerField(
         null=True,
         blank=True,
-        verbose_name='цена товара'
+        verbose_name='цена товара',
+        help_text='Стоимость товара'
     )
     description = models.TextField(
         null=True,
         blank=True,
-        verbose_name='описание товара'
+        verbose_name='описание товара',
+        help_text='Подробное описание товара'
     )
     author = models.ForeignKey(
         User,
@@ -19,11 +30,13 @@ class Ad(models.Model):
         verbose_name='владелец',
         related_name='advertisement',
         null=True,
-        blank=True
+        blank=True,
+        help_text='Пользователь разместивший объявление'
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
-        verbose_name='дата создания'
+        verbose_name='дата создания',
+        help_text='Дата и время создания объявления'
     )
     image = models.ImageField(
         upload_to='ads/',
@@ -33,6 +46,9 @@ class Ad(models.Model):
     )
 
     def __str__(self):
+        """
+        Возвращает заголовок объявления как строковое представление объекта.
+        """
         return self.title
 
     class Meta:
@@ -41,18 +57,23 @@ class Ad(models.Model):
 
 
 class Comment(models.Model):
-    text = models.TextField(verbose_name='текст')
+    """
+    Модель, представляющая комментарии к объявлению.
+    """
+    text = models.TextField(verbose_name='текст', help_text='Текст комментария')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         verbose_name='владелец',
         related_name='comments',
         null=True,
-        blank=True
+        blank=True,
+        help_text='Пользователь, оставивший комментарий'
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
-        verbose_name='дата создания'
+        verbose_name='дата создания',
+        help_text='Дата и время создания комментария'
     )
     ad = models.ForeignKey(
         Ad,
@@ -60,13 +81,16 @@ class Comment(models.Model):
         verbose_name='объявление',
         related_name='comments',
         null=True,
-        blank=True
+        blank=True,
+        help_text='Объявление, к которому относится комментарий'
     )
 
     def __str__(self):
+        """
+        Возвращает текст комментария как строковое представление объекта.
+        """
         return self.text
 
     class Meta:
         verbose_name = 'отзыв'
         verbose_name_plural = 'отзывы'
-
--- users/models.py
+++ users/models.py
@@ -2,27 +2,51 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from .managers import UserManager
+
 
 class UserRoles(models.TextChoices):
+    """
+    Перечисление возможных ролей пользователя.
+    """
     USER = 'user', _('user')
     ADMIN = 'admin', _('admin')
 
 
 class User(AbstractUser):
+    """
+    Кастомная модель пользователя,
+    где email используется как уникальный
+    идентификатор для аутентификации.
+    """
     username = None
     email = models.EmailField(_('email address'), unique=True)
     role = models.CharField(max_length=5, choices=UserRoles.choices,
-                            default=UserRoles.USER, verbose_name='Роль')
+                            default=UserRoles.USER, verbose_name='Роль',
+                            help_text='Роль пользователя в системе')
     image = models.ImageField(
         upload_to='users/',
         null=True,
         blank=True,
-        verbose_name='Аватар'
+        verbose_name='Аватар',
+        help_text='Аватар пользователя'
     )
-    phone = models.CharField(max_length=35, verbose_name='Телефон', blank=True, null=True)
+    phone = models.CharField(
+        max_length=35,
+        verbose_name='Телефон',
+        blank=True,
+        null=True,
+        help_text='Номер телефона пользователя'
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'phone']
 
+    objects = UserManager()
+
     def __str__(self):
+        """
+        Возвращает email пользователя
+        как строковое представление объекта.
+        """
         return self.email
